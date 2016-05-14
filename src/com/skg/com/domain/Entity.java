2bee8c0f54d14b0125970c86ef283bdfec121303
zzhyouyou <625828058@qq.com>
2016-05-14 11:15:55 +0800

    新类

---
+++ src/com/skg/com/domain/Entity.java
@@ -0,0 +1,19 @@
+package com.skg.com.domain;
+
+public class Entity {
+	private String name;
+	private String sex;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+}
